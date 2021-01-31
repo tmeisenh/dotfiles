f5161e09dd97ff499cd131dad9029fa9d4d6eef0
Travis Meisenheimer <travis@indexoutofbounds.com>
2021-01-30 23:25:31 -0600

    Updated plugins

--- bin/vscode-setup.sh
+++ bin/vscode-setup.sh
@@ -10,39 +10,42 @@ fi
 echo
 
 EXTENSION=(
-    andys8.jest-snippets
-    christian-kohler.npm-intellisense
-    cssho.vscode-svgviewer
-    dbaeumer.vscode-eslint
-    eamodio.gitlens
-    EditorConfig.EditorConfig
-    Equinusocio.vsc-community-material-theme
-    Equinusocio.vsc-material-theme
-    equinusocio.vsc-material-theme-icons
-    esbenp.prettier-vscode
-    foxundermoon.shell-format
-    humao.rest-client
-    johnpapa.vscode-peacock
-    jpoissonnier.vscode-styled-components
-    karigari.chat
-    ms-azuretools.vscode-docker
-    ms-dotnettools.csharp
-    ms-vscode-remote.remote-containers
-    ms-vscode.cpptools
-    ms-vsliveshare.vsliveshare
-    ms-vsliveshare.vsliveshare-audio
-    ms-vsliveshare.vsliveshare-pack
-    naumovs.color-highlight
-    rbbit.typescript-hero
-    shd101wyy.markdown-preview-enhanced
-    skyran.js-jsx-snippets
-    streetsidesoftware.code-spell-checker
-    stylelint.vscode-stylelint
-    tomphilbin.gruvbox-themes
-    yzhang.markdown-all-in-one)
+andys8.jest-snippets
+christian-kohler.npm-intellisense
+cssho.vscode-svgviewer
+dbaeumer.vscode-eslint
+eamodio.gitlens
+EditorConfig.EditorConfig
+Equinusocio.vsc-community-material-theme
+Equinusocio.vsc-material-theme
+equinusocio.vsc-material-theme-icons
+esbenp.prettier-vscode
+foxundermoon.shell-format
+humao.rest-client
+jmfirth.vsc-space-block-jumper
+johnpapa.vscode-peacock
+jpoissonnier.vscode-styled-components
+karigari.chat
+mattn.Lisp
+ms-azuretools.vscode-docker
+ms-dotnettools.csharp
+ms-vscode-remote.remote-containers
+ms-vscode.cpptools
+ms-vsliveshare.vsliveshare
+ms-vsliveshare.vsliveshare-audio
+ms-vsliveshare.vsliveshare-pack
+naumovs.color-highlight
+rbbit.typescript-hero
+shd101wyy.markdown-preview-enhanced
+skyran.js-jsx-snippets
+streetsidesoftware.code-spell-checker
+stylelint.vscode-stylelint
+tomphilbin.gruvbox-themes
+vscodevim.vim
+yzhang.markdown-all-in-one)
 
 echo Installing extensions
 for e in ${EXTENSION[@]}; do
-    echo $e
+# loop this way so if there's an error we just continue to the next ;)
     code --install-extension $e
 done
