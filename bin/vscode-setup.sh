75e1c7c29af48ad61420e8ea504eb77c22d09690
Travis Meisenheimer <travis@indexoutofbounds.com>
2021-01-31 12:19:03 -0600

    Configures VSpaceCode - spacemacs for vscode

--- bin/vscode-setup.sh
+++ bin/vscode-setup.sh
@@ -32,9 +32,6 @@ ms-azuretools.vscode-docker
 ms-dotnettools.csharp
 ms-vscode-remote.remote-containers
 ms-vscode.cpptools
-ms-vsliveshare.vsliveshare
-ms-vsliveshare.vsliveshare-audio
-ms-vsliveshare.vsliveshare-pack
 naumovs.color-highlight
 rbbit.typescript-hero
 shd101wyy.markdown-preview-enhanced
@@ -42,11 +39,17 @@ skyran.js-jsx-snippets
 streetsidesoftware.code-spell-checker
 stylelint.vscode-stylelint
 tomphilbin.gruvbox-themes
-vscodevim.vim
+VSpaceCode.vspacecode
 yzhang.markdown-all-in-one)
 
+echo "Removing all old extensions"
+rm -rf ~/.vscode/extensions
+
+echo "Installing new extensions"
 echo Installing extensions
 for e in ${EXTENSION[@]}; do
 # loop this way so if there's an error we just continue to the next ;)
     code --install-extension $e
 done
+echo "Done installing extensions.  Check that all extensions are enabled (some aren't for whatever reason)."
+echo "If installing for the first time, configure the spacemacs keybindings in vscode 'VSpaceCode: Configure Default Settings and Keybindings'"
