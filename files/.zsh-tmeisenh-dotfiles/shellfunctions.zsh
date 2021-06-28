2401fc444d2406d71752e1bc920f65d4e94b9f71
Travis Meisenheimer <travis@indexoutofbounds.com>
2021-06-28 08:09:53 -0500

    some updates

--- files/.zsh-tmeisenh-dotfiles/shellfunctions.zsh
+++ files/.zsh-tmeisenh-dotfiles/shellfunctions.zsh
@@ -152,6 +152,3 @@ function home_status() {
   (cd ~/git/dotfiles; git st)
 }
 
-function load_keys() {
-  find ~/.ssh -type f -name "*.pub" | xargs -L 1 -n 1 -I echo  | sed 's/\.pub//g' | ssh-add -K
-}
