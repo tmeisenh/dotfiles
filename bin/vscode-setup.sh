76370a743e10e23385df3b81cbf636479b462212
Travis Meisenheimer <travis@indexoutofbounds.com>
2022-01-10 09:59:10 -0600

    removed spacemacs plugin for lack of use

--- bin/vscode-setup.sh
+++ bin/vscode-setup.sh
@@ -10,37 +10,37 @@ fi
 echo
 
 EXTENSION=(
-andys8.jest-snippets
-christian-kohler.npm-intellisense
-Compulim.vscode-clock
-cssho.vscode-svgviewer
-dbaeumer.vscode-eslint
-eamodio.gitlens
-EditorConfig.EditorConfig
-Equinusocio.vsc-community-material-theme
-Equinusocio.vsc-material-theme
-equinusocio.vsc-material-theme-icons
-esbenp.prettier-vscode
-foxundermoon.shell-format
-humao.rest-client
-jmfirth.vsc-space-block-jumper
-johnpapa.vscode-peacock
-jpoissonnier.vscode-styled-components
-karigari.chat
-mattn.Lisp
-ms-azuretools.vscode-docker
-ms-dotnettools.csharp
-ms-vscode-remote.remote-containers
-ms-vscode.cpptools
-naumovs.color-highlight
-rbbit.typescript-hero
-shd101wyy.markdown-preview-enhanced
-skyran.js-jsx-snippets
-streetsidesoftware.code-spell-checker
-stylelint.vscode-stylelint
-tomphilbin.gruvbox-themes
-VSpaceCode.vspacecode
-yzhang.markdown-all-in-one)
+    andys8.jest-snippets
+    christian-kohler.npm-intellisense
+    Compulim.vscode-clock
+    cssho.vscode-svgviewer
+    dbaeumer.vscode-eslint
+    eamodio.gitlens
+    EditorConfig.EditorConfig
+    Equinusocio.vsc-community-material-theme
+    Equinusocio.vsc-material-theme
+    equinusocio.vsc-material-theme-icons
+    esbenp.prettier-vscode
+    foxundermoon.shell-format
+    hashicorp.terraform
+    jmfirth.vsc-space-block-jumper
+    johnpapa.vscode-peacock
+    jpoissonnier.vscode-styled-components
+    karigari.chat
+    mattn.Lisp
+    ms-azuretools.vscode-docker
+    ms-dotnettools.csharp
+    ms-vscode-remote.remote-containers
+    ms-vscode.cpptools
+    naumovs.color-highlight
+    rbbit.typescript-hero
+    shd101wyy.markdown-preview-enhanced
+    skyran.js-jsx-snippets
+    streetsidesoftware.code-spell-checker
+    stylelint.vscode-stylelint
+    tomphilbin.gruvbox-themes
+    vscodevim.vim
+    yzhang.markdown-all-in-one)
 
 echo "Removing all old extensions"
 rm -rf ~/.vscode/extensions
@@ -48,7 +48,7 @@ rm -rf ~/.vscode/extensions
 echo "Installing new extensions"
 echo Installing extensions
 for e in ${EXTENSION[@]}; do
-# loop this way so if there's an error we just continue to the next ;)
+    # loop this way so if there's an error we just continue to the next ;)
     code --install-extension $e
 done
 echo "Done installing extensions.  Check that all extensions are enabled (some aren't for whatever reason)."
