74e40edb89a580b338e225e36a9db44eace79da6
Travis Meisenheimer <travis@indexoutofbounds.com>
2023-02-05 11:42:29 -0600

    updated timemachine to work with nas

--- files/bin-darwin/timemachine.sh
+++ files/bin-darwin/timemachine.sh
@@ -6,29 +6,33 @@
 # uses https://github.com/tmeisenh/linux-timemachine (my fork)
 # To have the script run automatically by launchd, install in ~/Library/LaunchAgents
 
-export PATH=/usr/local/bin:/usr/bin:/bin
-LHOME="/Users/travis"
-command="$LHOME/git/linux-timemachine/timemachine"
-rsync_dir="/Volumes/backups"
+###
+# NOTE: When doing `du -csh` on non-nas the output is incorrect.  The command run ssh'd into the nas reports the correct size.
+###
 
-if [ ! -f "$command" ]; then
-  echo "$command is not installed"
+export PATH=/usr/local/bin:/usr/bin:/bin
+MYHOME="/Users/travis"
+command="${MYHOME}/git/linux-timemachine/timemachine"
+rsync_loc="nasreader:/volume1/backups"
+rsync_args="--progress --no-perms --no-owner --no-group"
+
+if [ ! -f "${command}" ]; then
+  echo "${command} is not installed"
   exit 1
 fi
 
-echo "Starting at `date`"
-#$command "$LHOME" "$rsync_dir/home" -- --exclude-from="$LHOME/.timemachine.exclude.txt" --progress
-$command "$LHOME/Documents" "$rsync_dir/Documents" -- --progress --no-perms --no-owner --no-group 
-#$command "$LHOME/Music" "$rsync_dir/Music" -- --progress --no-perms --no-owner --no-group 
+echo "Starting at $(date)"
+$command "--debug" "${MYHOME}/Documents" "${rsync_loc}/Documents" -- "${rsync_args}"
+#$command "${MYHOME}/Music" "${rsync_loc}/Music" -- "${rsync_args}"
 ret="$?"
 
 if [ $ret -ne 0 ]; then
   echo "Did you enable Full disk access?"
-  echo "Finished at `date`"
+  echo "Finished at $(date)"
   echo "--------------------------------------------------------------------------------"
   exit $ret
 fi
 
-echo "Finished at `date`"
+echo "Finished at $(date)"
 echo "--------------------------------------------------------------------------------"
 exit 0
